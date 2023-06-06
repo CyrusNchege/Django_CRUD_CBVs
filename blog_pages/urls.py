e1d47f5e01fa1f7f344b361b50f43b25308b8ab1
CyrusNchege <cyruschegecc@gmail.com>
2023-06-06 17:53:11 +0300

    Add create view using cbvs

--- blog_pages/urls.py
+++ blog_pages/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.BlogListView.as_view(), name= 'blog_list'),
     path('<int:pk>/', views.BlogDetailView.as_view(), name= "blog_detail"),
+    path('create/', views.BlogCreateView.as_view(), name='blog_create')
 ]
--- blog_pages/views.py
+++ blog_pages/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView,  UpdateView, DeleteView
 from .models import Post
+from .forms import PostCreateForm
+from django.urls  import reverse_lazy
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post
+    queryset = Post.objects.all()
     template_name = "blog/list.html"
     context_object_name = 'blogs'
 
 class BlogDetailView(DetailView):
-    model = Post
+    queryset = Post.objects.all()
     template_name = "blog/task_detail.html"
     context_object_name = 'blogs'
    
+class BlogCreateView(CreateView):
+    model = Post
+    form_class = PostCreateForm
+    template_name = "blog/task_create.html"
+    success_url = reverse_lazy('blog:blog_list')
