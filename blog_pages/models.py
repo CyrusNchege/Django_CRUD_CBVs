72cee31266554e141c3780223e7d7fa26ac45846
CyrusNchege <cyruschegecc@gmail.com>
2023-06-06 13:33:28 +0300

    Implemeted list views using cbvs'

--- blog_pages/models.py
+++ blog_pages/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.title
--- blog_pages/views.py
+++ blog_pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView,  UpdateView, DeleteView
+from .models import Post
 
 # Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = "blog/list_detail.html"
+    context_object_name = 'tasks'
